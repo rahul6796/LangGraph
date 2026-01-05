8aa698477f9395e7585c0f79bdfaecb2929f5793
Rahul Parjapati <60359760+rahul6796@users.noreply.github.com>
2026-01-05 16:51:59 +0530

    chatbot.db
    streamlit ui with threading

--- chatbot/streamlit_ui_threading.py
+++ chatbot/streamlit_ui_threading.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langgraph_backend import chatbot
+from langgraph_tool_backend import chatbot
 from langchain_core.messages import HumanMessage, AIMessage
 import uuid
 
@@ -80,7 +80,14 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+    # CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+    CONFIG = {
+        "configurable": {"thread_id": st.session_state["thread_id"]},
+        "metadata": {
+            "thread_id": st.session_state["thread_id"]
+        },
+        "run_name": "chat_turn",
+    }
 
      # first add the message to message_history
     with st.chat_message("assistant"):
